7907cbd4f3583d958d9e935781e19c58f2bf4245
cesslab <anwarruff@gmail.com>
2018-10-17 17:17:27 -0400

    added subsession lottery spec

--- auction/models.py
+++ auction/models.py
@@ -3,23 +3,28 @@ from otree.api import (
     Currency as c, currency_range
 )
 
-
-author = 'Your name here'
-
-doc = """
-Your app description
-"""
+from experiment.lottery import LotterySpecification
+from experiment.participant import Participant
+from experiment.auction import Auction
 
 
 class Constants(BaseConstants):
     name_in_url = 'auction'
-    players_per_group = 3
-    num_rounds = 1
+    players_per_group = 4
+    rounds_per_lottery = 2
+    num_rounds = 4
 
 
 class Subsession(BaseSubsession):
     def creating_session(self):
         self.group_randomly()
+
+        if self.round_number == 1:
+            lottery_specs = [
+                LotterySpecification(1, 5, 0.5, 2),
+                LotterySpecification(10, 20, 0.5, 2)]
+            for player in self.get_players():
+                Participant.set_experiment(player, Auction(Constants.rounds_per_lottery, lottery_specs))
 
 
 class Group(BaseGroup):
