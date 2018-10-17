508a973daa9b059d3814e601cc322b21d293274a
cesslab <anwarruff@gmail.com>
2018-10-17 12:16:51 -0400

    created lottery realized_value

--- experiment/lotteryspecification.py
+++ experiment/lotteryspecification.py
@@ -3,6 +3,7 @@ import random
 
 class LotterySpecification:
     def __init__(self, alpha: int, beta: int, p: float, epsilon: float):
+        assert(epsilon <= beta)
         self.alpha = alpha
         self.beta = beta
         self.p = p
@@ -11,14 +12,12 @@ class LotterySpecification:
 
 class Lottery:
     def __init__(self, spec: LotterySpecification):
-        # random value used to determine whether the value will be zero
-        # or a value between alpha and beta, inclusively.
-        self.r1 = random.random()
-        self.r2 = random.randint(spec.alpha, spec.beta)
-        if self.r1 <= (1 - spec.p):
-            self.value = 0
-        else:
-            self.value = self.r2
-
+        self.value = random.randint(spec.alpha, spec.beta)
         self.low_signal = self.value - spec.epsilon
         self.high_signal = self.value + spec.epsilon
+
+        self.r1 = random.random()
+        if self.r1 <= (1 - spec.p):
+            self.realized_value = 0
+        else:
+            self.realized_value = self.value
