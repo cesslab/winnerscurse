7883ab267a3978932092e2e89efacfbf7f0c645f
cesslab <anwarruff@gmail.com>
2018-10-17 11:49:59 -0400

    created lottery class along with constructor

--- experiment/lotteryspecification.py
+++ experiment/lotteryspecification.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 
 
 class LotterySpecification:
@@ -8,3 +8,17 @@ class LotterySpecification:
         self.p = p
         self.epsilon = epsilon
 
+
+class Lottery:
+    def __init__(self, spec: LotterySpecification):
+        # random value used to determine whether the value will be zero
+        # or a value between alpha and beta, inclusively.
+        self.r1 = random.random()
+        self.r2 = random.randint(spec.alpha, spec.beta)
+        if self.r1 <= (1 - spec.p):
+            self.value = 0
+        else:
+            self.value = self.r2
+
+        self.low_signal = self.value - spec.epsilon
+        self.high_signal = self.value + spec.epsilon
