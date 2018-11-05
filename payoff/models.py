64f7e7f6d516bac7c7e9a77edc98934cd030357f
cesslab <anwarruff@gmail.com>
2018-11-05 17:37:23 -0500

    Added payoff for phase 2

--- payoff/models.py
+++ payoff/models.py
@@ -1,14 +1,11 @@
+import random
+
 from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
 
-
-author = 'Your name here'
-
-doc = """
-Your app description
-"""
+from experiment.lottery import RedBlueLottery
 
 
 class Constants(BaseConstants):
@@ -26,4 +23,51 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    task_id = models.IntegerField()
+    lottery_type = models.IntegerField()
+    random_cutoff = models.FloatField()
+    random_chip_id = models.IntegerField()
+    num_red_chips = models.IntegerField()
+    num_blue_chips = models.IntegerField()
+    red_chosen = models.BooleanField()
+    phase_two_earnings = models.FloatField()
+    earnings = models.FloatField()
+    realized_value = models.FloatField()
+    play_lottery = models.BooleanField()
+
+    def player_dice_phase_payoffs(self):
+        lottery: RedBlueLottery = self.player.participant.vars["red_blue_lottery"]
+        self.task_id = self.player.participant.vars["die_side"]
+        self.random_cutoff = random.randint(lottery.min_cutoff, lottery.max_cutoff)
+
+        self.play_lottery = self.random_cutoff < self.player.participant.vars["cutoff"]
+
+        if not self.play_lottery:
+            self.earnings = float(self.random_cutoff)
+
+        self.lottery_type = lottery.ltype
+        if self.lottery_type == RedBlueLottery.ALL_KNOWN:
+            self.random_chip_id = random.randint(1, lottery.total)
+            self.num_red_chips = lottery.number_red
+            self.num_blue_chips = lottery.number_blue
+            self.red_chosen = self.random_chip_id <= self.num_red_chips
+
+        elif self.lottery_type == RedBlueLottery.COMPOUND_RISK:
+            self.num_red_chips = random.randint(0, lottery.total)
+            self.num_blue_chips = lottery.total - self.num_red_chips
+            self.random_chip_id = random.randint(1, lottery.total)
+            self.red_chosen = self.random_chip_id <= self.num_red_chips
+        else:
+            self.random_chip_id = random.randint(1, lottery.total)
+            self.num_red_chips = lottery.number_red
+            self.num_blue_chips = lottery.number_blue
+            self.red_chosen = self.random_chip_id <= self.num_red_chips
+
+        if self.red_chosen and self.player.participant.vars["bet"] == RedBlueLottery.BET_HIGH_RED:
+            self.realized_value = lottery.high_value
+        else:
+            self.realized_value = lottery.low_value
+
+        if self.play_lottery:
+            self.earnings = self.realized_value
+
--- payoff/pages.py
+++ payoff/pages.py
@@ -2,23 +2,48 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
-
-class MyPage(Page):
-    pass
+from experiment.lottery import RedBlueLottery
 
 
-class ResultsWaitPage(WaitPage):
-
+class PayoffCalculationWaitPage(WaitPage):
     def after_all_players_arrive(self):
-        pass
+        for player in self.group.get_players():
+            player.set_dice_payoff()
 
 
-class Results(Page):
-    pass
+class MethodThreeResultsPage(Page):
+    def vars_for_template(self):
+        lotteries = self.player.participant.vars['RedBlueLotteries']
+        lottery: RedBlueLottery = self.player.participant.vars["red_blue_lottery"]
+        lottery_ids = [lid for lid, value in lotteries.items()]
+
+        die_labels = self.player.participant.vars['die_labels']
+        die_lottery_ids = list(zip(die_labels, lottery_ids))
+
+        context = {
+            'rolled_side': self.player.participant.vars["die_side"],
+            'rolled_side_encoded': die_labels[self.player.task_id-1],
+            'die_encoding': die_lottery_ids,
+            'bet_color': self.player.bet,
+            'bet_high_red': RedBlueLottery.BET_HIGH_RED,
+            'bet_high_blue': RedBlueLottery.BET_HIGH_BLUE,
+            'high_value': lottery.high_value,
+            'low_value': lottery.low_value,
+            'lottery': lottery,
+            'lottery_type': lottery.ltype,
+            'cutoff': self.player.participant.vars["cutoff"],
+            'random_cutoff': self.player.random_cutoff,
+            'play_lottery': self.player.play_lottery,
+            'num_red': self.player.num_red_chips,
+            'num_blue': self.player.num_blue_chips,
+            'realized_value': self.player.realized_value,
+            'earnings': self.player.earnings
+        }
+
+        return context
 
 
 page_sequence = [
-    MyPage,
-    ResultsWaitPage,
-    Results
+    PayoffCalculationWaitPage,
+    MethodThreeResultsPage,
 ]
