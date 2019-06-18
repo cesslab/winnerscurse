fa2212f4620099fc9cff6985d8fa183280632d27
cesslab <anwarruff@gmail.com>
2019-06-18 15:20:59 -0400

    re-randomized round selected for payment

--- expected/models.py
+++ expected/models.py
@@ -53,8 +53,8 @@ class Subsession(BaseSubsession):
                 num_phase_one_valuations = Constants.num_lottery_types
                 num_phase_two_stage_one_valuations = AuctionConstants.num_lottery_types
                 num_phase_two_stage_two_valuations = AuctionConstants.num_rounds
-                total_valuations = num_phase_one_valuations + num_phase_two_stage_one_valuations + num_phase_two_stage_two_valuations
-                rround = 4
+                total_rounds = num_phase_one_valuations + num_phase_two_stage_one_valuations + num_phase_two_stage_two_valuations
+                rround = random.randint(1, total_rounds)
                 player.participant.vars["part_1_2_payment_round"] = rround
                 print("Payment: payment round = {}".format(rround))
 
